ef695b25accaa75ed2dbbf2580d4b3ba2d36a774
Laurent Perrinet <laurent.perrinet@gmail.com>
2018-04-04 20:09:36 +0200

    adding notes

--- 2018-04-05_BCP_talk.py
+++ 2018-04-05_BCP_talk.py
@@ -464,8 +464,8 @@ Indeed, such a model already exists, the onlin BCP, and we will adapt it for our
     title = meta['sections'][i_section-1]
 
     s.add_slide(content=s.content_figures(
-    [os.path.join(figpath_GDR, 'exp.png')],
-            title=title, height=s.meta['height']*.825),
+    [os.path.join(figpath_aSPEM, 'Experiment_randomblock.png')],
+            title=title + ' - Random-length block design', height=s.meta['height']*.825) + url,
     notes="""
 Let's remember our hierarchical generative model.
 
@@ -478,6 +478,32 @@ this new variable will be used to test different hypothesis which will be evalua
 in our case, we will assume that the bayesian model knows about the structure of the generative model and we will set it to the current run-length $r$, that is, at any given trial, the hypothesis that the past r observations belong to the same block. of course a wrong choice of a latent variables (let's say the temperture in the experimental room) may give unexpected results, even is the bayesian model is "optimal" - an essential point to understand in bayesian inference
 
     """)
+
+    s.add_slide(content="""
+Bayesian Online Changepoint Detector
+------------------------------------
+
+* an implementation of
+[Adams &amp; MacKay 2007 "Bayesian Online Changepoint Detection"](http://arxiv.org/abs/0710.3742)
+in Python.
+
+````
+@TECHREPORT{ adams-mackay-2007,
+AUTHOR = "Ryan Prescott Adams and David J.C. MacKay",
+TITLE  = "Bayesian Online Changepoint Detection",
+INSTITUTION = "University of Cambridge",
+ADDRESS = "Cambridge, UK",
+YEAR = "2007",
+NOTE = "arXiv:0710.3742v1 [stat.ML]",
+URL = "http://arxiv.org/abs/0710.3742"
+}
+````
+
+* adapted from https://github.com/JackKelly/bayesianchangepoint by Jack Kelly (2013) for a binomial input.
+
+* This code is based on the  [MATLAB implementation](http://www.inference.phy.cam.ac.uk/rpa23/changepoint.php) provided by Ryan Adam. available at http://hips.seas.harvard.edu/content/bayesian-online-changepoint-detection
+
+""", notes='', md=True)
 
     tag = 'bcp_model_layer_' #  'model_bcp_'
     blobs = ["""
@@ -521,83 +547,60 @@ First, we initialize the first node to prior values
     [os.path.join(figpath_aSPEM, tag + txt + '.png')],
                 title=title, height=s.meta['height']*.775)+blob,
        notes=notes_)
+
     # https://raw.githubusercontent.com/laurentperrinet/bayesianchangepoint/master/README.md
-    for text, md in zip(["""
-<h2>Bayesian Changepoint Detector</h2>
 
-<ol>
-<li> Initialize
-</li>
- <ul>
-  <li>
-   $P(r_0)= S(r)$ or $P(r_0=0)=1$ and
- </li>
-  <li>
-   $ν^{(0)}_1 = ν_{prior}$ and $χ^{(0)}_1 = χ_{prior}$
- </li>
-  </ul>
- <li>
- Observe New Datum $x_t$
-</li>
- <li>
-  Evaluate Predictive Probability $π_{1:t} = P(x_t |ν^{(r)}_t,χ^{(r)}_t)$
-</li>
- <li>
-  Calculate Growth Probabilities $P(r_t=r_{t-1}+1, x_{1:t}) = P(r_{t-1}, x_{1:t-1}) \cdot π^{(r)}_t \cdot (1−H(r^{(r)}_{t-1}))$
-</li>
- <li>
-  Calculate Changepoint Probabilities $P(r_t=0, x_{1:t})= \sum_{r_{t-1}} P(r_{t-1}, x_{1:t-1}) \cdot π^{(r)}_t \cdot H(r^{(r)}_{t-1})$
-</li>
- <li>
-  Calculate Evidence $P(x_{1:t}) = \sum_{r_{t-1}} P (r_t, x_{1:t})$
-</li>
- <li>
-  Determine Run Length Distribution $P (r_t | x_{1:t}) = P (r_t, x_{1:t})/P (x_{1:t}) $
-</li>
- <li>
-  Update Sufficient Statistics :
-</li>
- <ul>
-  <li>
-   $ν^{(0)}_{t+1} = ν_{prior}$, $χ^{(0)}_{t+1} = χ_{prior}$
- </li>
-  <li>
-   $ν^{(r+1)}_{t+1} = ν^{(r)}_{t} +1$, $χ^{(r+1)}_{t+1} = χ^{(r)}_{t} + u(x_t)$
- </li>
-  </ul>
- <li>
-  Perform Prediction $P (x_{t+1} | x_{1:t}) =   P (x_{t+1}|x_{1:t} , r_t) \cdot P (r_t|x_{1:t})$
-</li>
- <li>
-  go to (2)
-</li>
- </ol>
-        ""","""
-Bayesian Changepoint Detector
------------------------------
+    s.add_slide(content="""
+    <h2>Bayesian Changepoint Detector</h2>
 
-* an implementation of
-[Adams &amp; MacKay 2007 "Bayesian Online Changepoint Detection"](http://arxiv.org/abs/0710.3742)
-in Python.
-
-````
-@TECHREPORT{ adams-mackay-2007,
-AUTHOR = "Ryan Prescott Adams and David J.C. MacKay",
-TITLE  = "Bayesian Online Changepoint Detection",
-INSTITUTION = "University of Cambridge",
-ADDRESS = "Cambridge, UK",
-YEAR = "2007",
-NOTE = "arXiv:0710.3742v1 [stat.ML]",
-URL = "http://arxiv.org/abs/0710.3742"
-}
-````
-
-* adapted from https://github.com/JackKelly/bayesianchangepoint by Jack Kelly (2013) for a binomial input.
-
-* This code is based on the  [MATLAB implementation](http://www.inference.phy.cam.ac.uk/rpa23/changepoint.php) provided by Ryan Adam. available at http://hips.seas.harvard.edu/content/bayesian-online-changepoint-detection
-
-"""], [False, True]):
-        s.add_slide(content=text, notes='', md=md)
+    <ol>
+    <li> Initialize
+    </li>
+     <ul>
+      <li>
+       $P(r_0)= S(r)$ or $P(r_0=0)=1$ and
+     </li>
+      <li>
+       $ν^{(0)}_1 = ν_{prior}$ and $χ^{(0)}_1 = χ_{prior}$
+     </li>
+      </ul>
+     <li>
+     Observe New Datum $x_t$
+    </li>
+     <li>
+      Evaluate Predictive Probability $π_{1:t} = P(x_t |ν^{(r)}_t,χ^{(r)}_t)$
+    </li>
+     <li>
+      Calculate Growth Probabilities $P(r_t=r_{t-1}+1, x_{1:t}) = P(r_{t-1}, x_{1:t-1}) \cdot π^{(r)}_t \cdot (1−H(r^{(r)}_{t-1}))$
+    </li>
+     <li>
+      Calculate Changepoint Probabilities $P(r_t=0, x_{1:t})= \sum_{r_{t-1}} P(r_{t-1}, x_{1:t-1}) \cdot π^{(r)}_t \cdot H(r^{(r)}_{t-1})$
+    </li>
+     <li>
+      Calculate Evidence $P(x_{1:t}) = \sum_{r_{t-1}} P (r_t, x_{1:t})$
+    </li>
+     <li>
+      Determine Run Length Distribution $P (r_t | x_{1:t}) = P (r_t, x_{1:t})/P (x_{1:t}) $
+    </li>
+     <li>
+      Update Sufficient Statistics :
+    </li>
+     <ul>
+      <li>
+       $ν^{(0)}_{t+1} = ν_{prior}$, $χ^{(0)}_{t+1} = χ_{prior}$
+     </li>
+      <li>
+       $ν^{(r+1)}_{t+1} = ν^{(r)}_{t} +1$, $χ^{(r+1)}_{t+1} = χ^{(r)}_{t} + u(x_t)$
+     </li>
+      </ul>
+     <li>
+      Perform Prediction $P (x_{t+1} | x_{1:t}) =   P (x_{t+1}|x_{1:t} , r_t) \cdot P (r_t|x_{1:t})$
+    </li>
+     <li>
+      go to (2)
+    </li>
+     </ol>
+            """, notes='', md=False)
     #
     # s.add_slide_summary(
     #         ['Go fullscreen using the f key',
@@ -621,7 +624,30 @@ URL = "http://arxiv.org/abs/0710.3742"
     """)
 
 
-    modes = ['max'] # 'expectation',#for mode in ['expectation']:#, 'max']:# for mode in ['expectation', 'max']:
+
+    #
+    # s.add_slide_summary(
+    #         ['Go fullscreen using the f key',
+    #          'You can have an overlook using the o key',
+    #          'Navigate using the arrow keys',
+    #          'see notes using $P(x) = \exp(i)$ the n key'
+    #           'You can have an overlook using the o key',
+    #           'Navigate using the arrow keys',
+    #           'see notes using the n key'],
+    #           title='The Bayesian Changepoint Algorithm', fragment=True,
+    #                       notes="""
+    # * and write notes using markdown
+    #
+    # """)
+
+    s.add_slide(content=s.content_figures(
+    [os.path.join(figpath_talkbcp, 'github.png')],
+            title=title, height=s.meta['height']*.825),
+    notes="""
+
+    """)
+
+    modes = ['max', 'fixed'] # 'expectation',#for mode in ['expectation']:#, 'max']:# for mode in ['expectation', 'max']:
     for mode in modes:
 
         figname = os.path.join(figpath_talkbcp, 'bayesianchangepoint_' + mode + '.png')
@@ -641,8 +667,32 @@ URL = "http://arxiv.org/abs/0710.3742"
         s.add_slide(content=s.content_figures([figname],
                     title=title, height=s.meta['height']*.825),
            notes="""
+Let's now see the application of our model to a simple synthetic example before applying it to the experimental protocol that we used in our two experiments
 
-        """)
+
+- we show two panels, one below which displays the value of the belief for the different run-length, and one above where we will show the resaulting prediction of the next outcome.
+we obtain for any given sequence different values at the given trial in the form of columns for any possible run-length: the belief,
+and the sufficient statistics for the beta distirbution which allow to provide with an estimate of the current probability
+
+- first, we show the value of probability, low probabilities are blueish while high probabilities. at every trial, the agent evluates the value for the different possible run lengths, generating a column. by showing all columns we generate this image which shows the evaluation along the sequence of trials.
+
+- second we show above the sequence of observations that were shown to the agent in a light black line. the read line gives an evaluation of the most probable a posteriori probability as the probability to hte run-length the maximum a posteriori belief on the differettn beliefs about run-lengths. using the estimate of the precision at this
+
+We remark two main observations:
+
+- first, beliefs grow at the beginning along a linear ridge, as we begin our model by assuming there was a switch at time 0. Then we observe that at a switch (hidden to the model), the model
+such that belief is more stronlgly diffused until the probability
+
+-second, we may use this information to read-out the information the most probable probability and the confidence interval as shown by the red dashed lines (.05, and .95)
+
+
+This is in contrast with a fixed length model, for which
+- the delay will always be similar
+- there is no dynamic upDATE OF THE INFERRD probability
+
+
+as a summary, for any given sequnce, we get an estimate of the probability given by the ideal observer. we will now see how we can apply that to our experiemntal protocol.
+""")
 
     s.close_section()
 
@@ -694,8 +744,18 @@ if do_section[i_section]:
             s.add_slide(content=s.content_figures([figname],
                         title=title +  ' - inference with BCP', height=s.meta['height']*.825),
                notes="""
+               Let's use our model on the different sequences that were generated in our experiments in the different blocks.
 
-            """)
+we the same arrangement of panels, we show below the dynamical evolution of beliefs and above the resulting readout from the model
+
+we see that as in the synthetic example above, there is a correct detection of switch after a short delay of a few trials
+
+in particular, from this correct detection, the value of the inferred probability approaches the true one as the number of observations increase in one subblock.
+
+again, we see that a fixed length model gives a similar output but with the two disadvantages described above
+
+Let's now see how this applies to our experimental results by comparing human observers to our bayesian agent.
+""")
     # tag = 'results_bayesianchangepoint_'
     # for txt in [tag + 'm', tag + 'e']:
     #     s.add_slide(content=s.content_figures(
@@ -712,18 +772,50 @@ if do_section[i_section]:
             [os.path.join(figpath_aSPEM, tag + '_' + txt + '.png')],
                     title=title +  ' - fit with BCP', height=s.meta['height']*.825),
            notes="""
+Among our 12 subjects, we show four representative examples. we will use the same figure as in the section with raw results
 
+but we superposed to our 2 variables, the value of the readout inferred probability along with the confidence interval.
+
+compared to the raw results which were using the true (hidden) probability, it seems qualitatively that it follows well the traces observed experimenetally
+- first, both have similar delays in ddetercting a switch, reflecting the diuffusion of probability
+- second, precisions seems to increase in bigger sub-blocks as a function of the inferred run-length
+
+as a result, the inferred probability as a function of time constitutes a useful regressor
         """)
 
     # TODO : average KDE
     tag = 'kde_mean'
-    for mode in modes:
+    for mode in modes: #['fixed', 'max', ]: #, 'expectation'
         s.add_slide(content=s.content_figures(
-            [os.path.join(figpath_ms, tag + '_' + txt + '_' + mode + '.png') for txt in ['v_a', 'p_bet']],
+            [os.path.join(figpath_aSPEM, tag + '_' + mode + '.png')],
                     title=title +  ' - fit with BCP', height=s.meta['height']*.7, transpose=False, fragment=True),
            notes="""
+We may finally wrap up results and the model and plot
+
+scatters plots are visually misleading as they do not show well the density of data points
+
+we therefore used a kernel density estimation which clearly show the relationship between the agent probability and that reported by human observers
+- on the right, we
+
+to summarize, we have shown that
+- there is a correlation in the anticiapatory response of eye movements in a volatile environment that is captured if we know the true probability
+- that a fixed length models captures some of this correlation, but that
+- our online bayesian changep[oint model better captures this correlation and that this may hint at the neural mechanisms used to anticipate in a dynamic environment
+
+perspectives:
+- interindividual differences
+- RL
 
         """)
+    # # TODO : average KDE
+    # tag = 'kde_mean'
+    # for txt in [str(k) for k in range(4)]:
+    #     s.add_slide(content=s.content_figures(
+    #         [os.path.join(figpath_aSPEM, tag + '_' + txt + '.png')],
+    #                 title=title +  ' - fit with BCP', height=s.meta['height']*.7, transpose=False, fragment=True),
+    #        notes="""
+    #
+    #     """)
     s.close_section()
 
 
